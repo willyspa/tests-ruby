868427f5283cecdfef79c9b0212fa14f72999beb
willyspa <armed95@hotmail.fr>
2019-01-14 22:03:27 +0100

    bug ajout de multiply et sum

--- lib/02_calculator.rb
+++ lib/02_calculator.rb
@@ -5,12 +5,32 @@ def add(temp_nb1,temp_nb2)
   return result
 end
 
-def sub(temp_nb1,temp_nb2)
+def substract(temp_nb1,temp_nb2)
   temp_nb1.to_i
   temp_nb2.to_i
   result=temp_nb1-temp_nb2
   return result
 end
 
+
+def sum (array)
+
+  result =0
+
+  for i in 0..array.length #j initialise i la boucle for me permet
+    result = result + array[i].to_i  #je charge dans result la valeur de array[i]
+  end
+
+    return result
+  end
+
+def multiply(nb1, nb2)
+  result = nb1*nb2
+  return result
+end
+
+array=[5,7,5,5]
+puts sum(array)
 puts add(5,5)
-puts sub(5,5)
+puts substract(5,5)
+puts multiply(6,6)
