518a936e5600f40322429be9355a68b930192ce0
willyspa <armed95@hotmail.fr>
2019-01-14 19:56:42 +0100

    ajout chrono

---
+++ lib/05_timer.rb
@@ -0,0 +1,30 @@
+def time_string(time)
+
+   return Time.at(time).utc.to_s[11..18]
+=begin
+  time.to_i
+  sec = 0
+  min = 0
+  heure = 0
+  chrono = ""
+for i in  0..time
+
+   if sec == 60
+     sec = 0
+     min += 1
+   elsif
+   min == 60
+       min = 0
+       heure += 1
+     end
+     sec += 1
+end
+     chrono = "#{heure}:#{min}:#{sec}"
+     return chrono
+
+   end
+=end
+end
+
+
+   puts time_string(79)
